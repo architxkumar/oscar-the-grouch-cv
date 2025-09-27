bb0ce2bd92eccb82c772d146b38897e1d0b9f0d5
Archit Kumar <architxkumar@proton.me>
2025-09-27 16:00:15 +0530

    refactor: update the app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class CVApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Oscar the grouch CV',
+      title: 'CV',
       home: HomeScreen(),
     );
   }
